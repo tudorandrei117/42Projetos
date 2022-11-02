7c4ee3fefa056659f614f85c27b43d28c5979f8a
Tudzz <81469736+tudorandrei117@users.noreply.github.com>
2022-11-02 01:07:46 +0000

    Add files via upload

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -3,38 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tburlacu <tburlacu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tudor <tudor@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 16:10:11 by tburlacu          #+#    #+#             */
-/*   Updated: 2022/10/31 16:11:51 by tburlacu         ###   ########.fr       */
+/*   Updated: 2022/11/02 00:57:54 by tudor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "libft.h"
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	size_t 	i;
-	void 	*destination;
+	size_t	i;
+	void	*destination;
 
 	i = nmemb * size;
-	if(!(destination))
-}
-
-
-
-
-
-
-/* void	*ft_calloc(size_t count, size_t size)
-{
-	size_t	tot_size;
-	void	*dst;
-
-	tot_size = size * count;
-	if (!(dst = malloc(tot_size)))
+	if (!(destination = malloc(i)))
+	{
 		return (0);
-	ft_memset(dst, 0, tot_size);
-	return (dst);
-} */
+	}
+	ft_memset(destination, 0, i);
+	return (destination);
+}
